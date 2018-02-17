84284965ea27aa2fb79500cde8388f8d855dd164
Andrew <hothack9@gmail.com>
2018-02-17 22:35:34 +0300

    Hello World from dev brancn again!

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
         System.out.println("Hello World from master branch!");
-        System.out.println("Hello World from dev brancn!")
+        System.out.println("Hello World from dev brancn!");
+        System.out.println("Hello World from dev brancn again!");
     }
 
 }
